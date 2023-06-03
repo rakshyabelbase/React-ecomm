a52bb0ddbcdc5429c21d0ac5da55703fd6d1e0ab
Rakshya Belbase <rakshyabelbase12@gmail.com>
2023-06-03 22:20:20 +0545

    Ecommerce API with Nodejs and mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import morgan from "morgan";
 import authRoutes from './routes/auth.js';
 import categoryRoutes from './routes/category.js';
+import productRoutes from './routes/product.js';
 
 dotenv.config();
 
@@ -23,7 +24,7 @@ mongoose
 
 app.use("/api", authRoutes);
 app.use("/api", categoryRoutes);
-
+app.use("/api", productRoutes);
 
 const port = process.env.PORT||8000;
 
